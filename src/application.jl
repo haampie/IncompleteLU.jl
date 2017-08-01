a8ac04c8c2233343efefc74492ffedbbebb25c19
Harmen Stoppels <harmenstoppels@gmail.com>
2017-08-01 14:21:26 +0200

    Comments

--- src/application.jl
+++ src/application.jl
@@ -1,24 +1,43 @@
+import Base: A_ldiv_B!
+
 function A_ldiv_B!(F::ILUFactorization, y::AbstractVector)
     transposed_backward_substitution!(F.U, y)
     forward_substitution_without_diag!(F.L, y)
 end
 
-function transposed_backward_substitution!(A::SparseMatrixCSC, y::AbstractVector)
-    @inbounds for col = A.n : -1 : 1
-        for idx = A.colptr[col + 1] - 1 : -1 : A.colptr[col] + 1
-            y[col] -= A.nzval[idx] * y[A.rowval[idx]]
+"""
+Applies in-place backward substitution with the U factor, under the assumptions: 
+
+1. U is stored transposed / row-wise
+2. U has no lower-triangular elements stored
+3. U has (nonzero) diagonal elements stored.
+"""
+function transposed_backward_substitution!(U::SparseMatrixCSC, y::AbstractVector)
+    @inbounds for col = U.n : -1 : 1
+
+        # Substitutions
+        for idx = U.colptr[col + 1] - 1 : -1 : U.colptr[col] + 1
+            y[col] -= U.nzval[idx] * y[U.rowval[idx]]
         end
 
-        y[col] /= A.nzval[A.colptr[col]]
+        # Final answer for y[col]
+        y[col] /= U.nzval[U.colptr[col]]
     end
 
     y
 end
 
-function forward_substitution_without_diag!(A::SparseMatrixCSC, y::AbstractVector)
-    @inbounds for col = 1 : A.n - 1
-        for idx = A.colptr[col] : A.colptr[col + 1] - 1
-            y[A.rowval[idx]] -= A.nzval[idx] * y[col]
+"""
+Applies in-place forward substitution with the L factor, under the assumptions:
+
+1. L is stored column-wise (unlike U)
+2. L has no upper triangular elements
+3. L has *no* diagonal elements
+"""
+function forward_substitution_without_diag!(L::SparseMatrixCSC, y::AbstractVector)
+    @inbounds for col = 1 : L.n - 1
+        for idx = L.colptr[col] : L.colptr[col + 1] - 1
+            y[L.rowval[idx]] -= L.nzval[idx] * y[col]
         end
     end
 
