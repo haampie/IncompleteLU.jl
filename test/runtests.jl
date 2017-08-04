4b312fa9fc42476b3f90d74c370c9473a75421c6
Harmen Stoppels <harmenstoppels@gmail.com>
2017-08-04 12:26:24 +0200

    Add insertion sort updatable sparse vector to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using ILU
 
 include("sorted_set.jl")
 include("linked_list.jl")
+include("insertion_sort_update_vector.jl")
 include("application.jl")
 include("update_vector.jl")
 include("crout_ilu.jl")
