24eaa17d636e6e42b15a6757b5740b5c8d16b70e
Harmen Stoppels <harmenstoppels@gmail.com>
2017-08-04 13:55:48 +0200

    Append InsertableSparseVector as column to SparseMatrixCSC

--- src/insertion_sort_update_vector.jl
+++ src/insertion_sort_update_vector.jl
@@ -1,24 +1,63 @@
-import Base: getindex, setindex!, indices
+import Base: getindex, setindex!, indices, convert
 
-struct InsertableSparseVector{Tv,Ti <: Integer}
+struct InsertableSparseVector{Tv,Ti<:Integer,N}
     values::Vector{Tv}
     indices::SortedSet{Ti}
 
-    InsertableSparseVector{Tv,Ti}(n::Ti) where {Ti <: Integer,Tv} = new(Vector{Tv}(n), SortedSet(n))
+    InsertableSparseVector{Tv,Ti,N}() where {Ti <: Integer,Tv,N} = new(Vector{Tv}(N), SortedSet(N))
 end
 
-InsertableSparseVector{Tv}(n::Ti) where {Ti <: Integer,Tv} = InsertableSparseVector{Tv,Ti}(n)
+InsertableSparseVector{Tv}(n::Ti) where {Ti <: Integer,Tv} = InsertableSparseVector{Tv,Ti,n}()
 
 @inline getindex(v::InsertableSparseVector{Tv,Ti}, idx::Ti) where {Tv,Ti} = v.values[idx]
 @inline setindex!(v::InsertableSparseVector{Tv,Ti}, value::Tv, idx::Ti) where {Tv,Ti} = v.values[idx] = value
 @inline indices(v::InsertableSparseVector) = convert(Vector, v.indices)
 
-@inline function add!(v::InsertableSparseVector{Tv,Ti}, a::Tv, idx::Ti) where {Tv,Ti}
-    if push!(v.indices, idx)
+function convert(::Type{Vector}, v::InsertableSparseVector{Tv,Ti,N}) where {Tv,Ti,N}
+    vals = zeros(Tv, N)
+    for index in v.indices
+        vals[index] = v.values[index]
+    end
+    return vals
+end
+
+@inline function add!(v::InsertableSparseVector{Tv,Ti}, a::Tv, idx::Ti, prev_idx::Ti) where {Tv,Ti}
+    if push!(v.indices, idx, prev_idx)
         v[idx] = a
     else
         v[idx] += a
     end
 
     v
+end
+
+function axpy!(a::Tv, A::SparseMatrixCSC{Tv,Ti}, column::Ti, start::Ti, y::InsertableSparseVector{Tv,Ti,N}) where {Tv,Ti,N}
+    prev_index::Ti = N + one(Ti)
+    
+    @inbounds for idx = start : A.colptr[column + 1] - 1
+        add!(y, a * A.nzval[idx], A.rowval[idx], prev_index)
+        prev_index = A.rowval[idx]
+    end
+
+    y
+end
+
+function append_col!(A::SparseMatrixCSC{Tv}, y::InsertableSparseVector{Tv,Ti,N}, j::Ti, drop = zero(real(Tv))) where {Tv,Ti,N}
+    
+    total = 0
+    
+    for row = y.indices
+        if abs(y[row]) ≥ drop || row == j
+            push!(A.rowval, row)
+            push!(A.nzval, y[row])
+            total += 1
+        end
+    end
+
+    A.colptr[j + 1] = A.colptr[j] + total
+    
+    # Reset
+    y.indices[N + 1] = N + 1
+
+    nothing
 end
--- test/insertion_sort_update_vector.jl
+++ test/insertion_sort_update_vector.jl
@@ -1,14 +1,44 @@
 using Base.Test
-import ILU: InsertableSparseVector, add!
+import ILU: InsertableSparseVector, add!, axpy!, append_col!
 
 @testset "Insertion sorted sparse vector" begin
     v = InsertableSparseVector{Float64}(10)
 
-    add!(v, 3.0, 6)
-    add!(v, 3.0, 3)
-    add!(v, 3.0, 3)
+    add!(v, 3.0, 6, 11)
+    add!(v, 3.0, 3, 11)
+    add!(v, 3.0, 3, 11)
 
     @test v[6] == 3.0
     @test v[3] == 6.0
     @test indices(v) == [3, 6]
+end
+
+@testset "Add column of SparseMatrixCSC" begin
+    v = InsertableSparseVector{Float64}(5)
+    A = sprand(5, 5, 1.0)
+    axpy!(2., A, 3, A.colptr[3], v)
+    axpy!(3., A, 4, A.colptr[4], v)
+    @test convert(Vector, v) == 2 * A[:, 3] + 3 * A[:, 4]
+end
+
+@testset "Append column to SparseMatrixCSC" begin
+    v = InsertableSparseVector{Float64}(5)
+    add!(v, 0.3, 1, 6)
+    add!(v, 0.009, 3, 6)
+    add!(v, 0.12, 4, 6)
+    add!(v, 0.007, 5, 6)
+
+    A = spzeros(5, 5)
+    append_col!(A, v, 1, 0.1)
+
+    # Test whether the column is copied correctly
+    # and the dropping rule is applied
+    @test A[1, 1] == 0.3
+    @test A[2, 1] == 0.0
+    @test A[3, 1] == 0.0
+    @test A[4, 1] == 0.12
+    @test A[5, 1] == 0.0
+
+    # Test whether the InsertableSparseVector is reset
+    @test iszero(convert(Vector, v))
 end
