49df54e4d5965c4059f300646d62bf3800f71dbf
Harmen Stoppels <harmenstoppels@gmail.com>
2017-08-04 17:12:30 +0200

    Bench row copying

---
+++ bench/inserted_stuff.jl
@@ -0,0 +1,48 @@
+module BenchInsertion
+
+using ILU
+using BenchmarkTools
+
+function do_stuff(A)
+    n = size(A, 1)
+    U_row = ILU.InsertableSparseVector{Float64}(n);
+    A_reader = ILU.RowReader(A)
+
+    for k = 1 : n
+        col = first_in_row(A_reader, k)
+
+        while is_column(col)
+            ILU.add!(U_row, nzval(A_reader, col), col, n + 1)
+            next_col = next_column(A_reader, col)
+            next_row!(A_reader, col)
+
+            if has_next_nonzero(A_reader, col) && nzrow(A_reader, col) ≤ col
+                enqueue_next_nonzero!(A_reader, col)
+            end
+
+            col = next_col
+        end
+
+        U_row.indices[n + 1] = n + 1
+    end
+
+    U_row
+end
+
+function wut(n = 1_000)
+    A = sprand(n, n, 10 / n) + 15I
+    @benchmark BenchInsertion.do_stuff($A)
+end
+
+function check_allocs(n = 100_000)
+    srand(1)
+    A = sprand(n, n, 10 / n) + 15I
+    do_stuff(A)
+    Profile.clear()
+    Profile.clear_malloc_data()
+    @profile do_stuff(A)
+end
+
+end
+
+# BenchInsertion.check_allocs()
