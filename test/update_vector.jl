8385446a2b1f51c3150f524ec24435291edb2caa
Harmen <harmenstoppels@gmail.com>
2017-08-05 11:02:46 +0200

    Add another test

--- test/update_vector.jl
+++ test/update_vector.jl
@@ -8,7 +8,7 @@ import ILU: SparseVectorAccumulator, add!, axpy!
         @test iszero(v.full)
     end
 
-    let 
+    @testset "Add to SparseVectorAccumulator" begin
         v = SparseVectorAccumulator{Float64}(3)
         add!(v, 1.0, 3)
         add!(v, 1.0, 3)
@@ -23,12 +23,34 @@ import ILU: SparseVectorAccumulator, add!, axpy!
         @test convert(Vector, v) == [0; 3.0; 2.0]
     end
 
-    let 
+    @testset "Add column of SparseMatrixCSC" begin
         # Copy all columns of a 
         v = SparseVectorAccumulator{Float64}(5)
         A = sprand(5, 5, 1.0)
         axpy!(2., A, 3, A.colptr[3], v)
         axpy!(3., A, 4, A.colptr[4], v)
         @test convert(Vector, v) == 2 * A[:, 3] + 3 * A[:, 4]
+    end
+
+    @testset "Append column to SparseMatrixCSC" begin
+        v = SparseVectorAccumulator{Float64}(5)
+        add!(v, 0.3, 1)
+        add!(v, 0.009, 3)
+        add!(v, 0.12, 4)
+        add!(v, 0.007, 5)
+
+        A = spzeros(5, 5)
+        append_col!(A, v, 1, 0.1)
+
+        # Test whether the column is copied correctly
+        # and the dropping rule is applied
+        @test A[1, 1] == 0.3
+        @test A[2, 1] == 0.0
+        @test A[3, 1] == 0.0
+        @test A[4, 1] == 0.12
+        @test A[5, 1] == 0.0
+
+        # Test whether the InsertableSparseVector is reset
+        @test iszero(convert(Vector, v))
     end
 end
