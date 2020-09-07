6ebfa6de5c4b7aae88bcb4ca10277cc6765e7b46
Felix Gerick <felixgerick@gmail.com>
2020-09-07 22:50:42 +0200

    add application tests

--- test/application.jl
+++ test/application.jl
@@ -4,7 +4,7 @@ using LinearAlgebra
 
 @testset "Backward substitution" begin
     function test_bw_substitution(A::SparseMatrixCSC)
-        x = rand(size(A, 1))
+        x = rand(eltype(A),size(A, 1))
         y = copy(x)
 
         forward_substitution_without_diag!(A, x)
@@ -16,11 +16,15 @@ using LinearAlgebra
     test_bw_substitution(sparse(tril(rand(10, 10), -1)))
     test_bw_substitution(tril(sprand(10, 10, .5), -1))
     test_bw_substitution(spzeros(10, 10))
+
+    test_bw_substitution(sparse(tril(rand(ComplexF64,10, 10), -1)))
+    test_bw_substitution(tril(sprand(ComplexF64,10, 10, .5), -1))
+    test_bw_substitution(spzeros(ComplexF64,10, 10))
 end
 
 @testset "Forward substitution" begin
     function test_fw_substitution(A::SparseMatrixCSC)
-        x = rand(size(A, 1))
+        x = rand(eltype(A),size(A, 1))
         y = copy(x)
 
         transposed_backward_substitution!(A, x)
@@ -32,12 +36,17 @@ end
     test_fw_substitution(sparse(tril(rand(10, 10)) + 10I))
     test_fw_substitution(tril(sprand(10, 10, .5) + 10I))
     test_fw_substitution(spzeros(10, 10) + 10I)
+
+
+    test_fw_substitution(sparse(tril(rand(ComplexF64,10, 10)) + 10I))
+    test_fw_substitution(tril(sprand(ComplexF64,10, 10, .5) + 10I))
+    test_fw_substitution(spzeros(ComplexF64,10, 10) + 10I)
 end
 
 @testset "ldiv!" begin
     function test_ldiv!(L, U)
         LU = ILUFactorization(L, U)
-        x = rand(size(LU.L, 1))
+        x = rand(eltype(L),size(LU.L, 1))
         y = copy(x)
         z = copy(x)
         w = copy(x)
@@ -55,11 +64,20 @@ end
     end
 
     test_ldiv!(tril(sprand(10, 10, .5), -1), tril(sprand(10, 10, .5) + 10I))
+    test_ldiv!(tril(sprand(ComplexF64,10, 10, .5), -1), tril(sprand(ComplexF64,10, 10, .5) + 10I))
 end
 
 @testset "nnz" begin
-    L = tril(sprand(10, 10, .5), -1)
-    U = tril(sprand(10, 10, .5)) + 10I
-    LU = ILUFactorization(L, U)
-    @test nnz(LU) == nnz(L) + nnz(U)
+    let
+        L = tril(sprand(10, 10, .5), -1)
+        U = tril(sprand(10, 10, .5)) + 10I
+        LU = ILUFactorization(L, U)
+        @test nnz(LU) == nnz(L) + nnz(U)
+    end
+    let
+        L = tril(sprand(ComplexF64,10, 10, .5), -1)
+        U = tril(sprand(ComplexF64,10, 10, .5)) + 10I
+        LU = ILUFactorization(L, U)
+        @test nnz(LU) == nnz(L) + nnz(U)
+    end
 end
